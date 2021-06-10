46b32180f8c6cbfbfc275edc4a5f6ac7627383bb
omaimah-sulaiman <omaymahshakhatrah@gmail.com>
2021-06-10 20:47:50 +0300

    save infos from tow form to lacal storage

--- js/join.js
+++ js/join.js
@@ -1,21 +1,108 @@
 let driver=document.getElementById('dr')
 let parnter=document.getElementById('pa')
 let driverForm=document.getElementById('driverForm')
+
 let parnterForm=document.getElementById('partnerForm')
 
 
 
 driver.addEventListener('click',function(e){
-    event.preventDefault();
+    e.preventDefault();
     parnter.style.display='none'
     driverForm.style.display='block'
 })
 
 
-parnter.addEventListener('click',function(){
-    event.preventDefault();
+parnter.addEventListener('click',function(e){
+    e.preventDefault();
     driver.style.display='none'
     parnterForm.style.display='block'
 })
 
 
+
+// add costructer function
+
+
+function DriverForm(firstName,lastName,phone,time){
+    
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.phone=phone;
+    this.time=time
+    
+    DriverForm.all.push(this)
+    
+}
+
+DriverForm.all=[]
+
+driverForm.addEventListener('submit',submitFormDriver)
+
+
+function submitFormDriver(event){
+    
+    event.preventDefault();
+    
+    const firstName=event.target.firstName.value;
+    const lastName=event.target.lastName.value
+    const phone=event.target.phone.value
+    const time=event.target.time.value
+    
+    // console.log(time,firstName,lastName,phone)
+    new DriverForm(firstName,lastName,phone,time);
+    
+    savingLs();
+   
+    
+    driverForm.reset();
+
+    
+}
+
+
+function savingLs(){
+
+    localStorage.setItem('drivers',JSON.stringify(DriverForm.all))
+}
+
+
+
+
+// PARTNER FORM 
+
+function PartnerForm(fname,lname,number,location,bestCofe){
+    this.fname=fname;
+    this.lname-lname;
+    this.number=number;
+    this.location=location;
+    this.bestCofe=bestCofe;
+
+    PartnerForm.all.push(this)
+
+}
+
+PartnerForm.all=[]
+
+parnterForm.addEventListener('submit',submitPartnerForm)
+function submitPartnerForm(event){
+
+event.preventDefault();
+
+const fname=event.target.fname.value;
+const lname=event.target.lname.value;
+const number=event.target.number.value;
+const location=event.target.location.value;
+const bestCofe=event.target.bestCofe.value;
+
+// console.log(fname,lname,number,location,bestCofe)
+
+new PartnerForm(fname,lname,number,location,bestCofe)
+
+localStorage.setItem('partners',JSON.stringify(PartnerForm.all))
+
+parnterForm.reset()
+
+
+}
+submitPartnerForm()
