dbc0ba7dc96d606c35647156321b0487598ca84f
bayan-hmaidy98 <83514326+bayan-hmaidy98@users.noreply.github.com>
2021-06-13 10:38:07 +0300

    Omiama (#45)
    * add structure to index page
    
    * add structur to index
    
    * delete home page
    
    * add such style to index
    
    * add to the joun us the structure in the  main
    
    * add styling to footer
    
    * make form shown or not depend user what he want
    
    * save infos from tow form to lacal storage
    
    * add tow message when finish from submiting
    
    * add thanks to user when he end the submit
    
    * add image to object and ton reset local storage when new submit
    
    * make more styling on join us page
    
    * anther style to header and footer
    
    * style
    
    * more styling and solve point in local storage
    
    Co-authored-by: omaimah-sulaiman <omaymahshakhatrah@gmail.com>

--- js/join.js
+++ js/join.js
@@ -116,6 +116,7 @@ new PartnerForm(fname,lname,number,location,bestCofe,imageFile)
 var array = JSON.parse(localStorage.getItem('Partner') || '[]');
 array.push(PartnerForm.all);
 localStorage.setItem('Partner', JSON.stringify(array));
+localStorage.removeItem('recentImage')
 
 // localStorage.setItem('partners',JSON.stringify(PartnerForm.all))
 
